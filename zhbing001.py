d4558b66bb6dbc00a9d381f97d9c94a8f4f8c79e
zhrbing <zhrbing@gmail.com>
2017-06-07 17:51:32 +0800

    generator生成器实现杨辉三角

--- zhbing001.py
+++ zhbing001.py
@@ -12,11 +12,61 @@
 # for i in l0:
 #     print(i,end='\t')
 
-def fib(max):
-    n, a, b = 1, 1, 1
-    while n <= max:
-        print('%3d' % a, end='\t')
-        a, b = b, a + b
-        n = n + 1
+# def fib1(max):
+#     n, a, b = 1, 1, 1
+#     while n <= max:
+#         print(a)
+#         a, b = b, a + b
+#         n = n + 1
+# fib1(5)
+#
+# def fib2(max):
+#     n, a, b = 1, 1, 1
+#     while n <= max:
+#         yield a
+#         a, b = b, a + b
+#         n = n + 1
+# print(fib2(3))
 
-fib(8)
+#######################################################
+# generator生成器实现杨辉三角
+def triangle_yang():
+    L=[1]
+    while True:
+        yield L
+        L=[L[x]+L[x+1] for x in range(len(L)-1)]
+        L=[1]+L+[1]
+
+n=0
+for i in triangle_yang():
+    print(i)
+    n=n+1
+    if n==10:
+        break
+####################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
