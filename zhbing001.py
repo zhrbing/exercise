f50a66c3489ab2aff3a2f2f81e8eebb7e808ecde
zhbing <zhrbing@gmail.com>
2017-06-13 13:16:00 +0800

    201706131315@Yintech

--- zhbing001.py
+++ zhbing001.py
@@ -91,19 +91,61 @@ from functools import reduce
 # l0=['apple','Microsoft','google','IBM','Nokia']
 # print(sorted(l0,key=str.lower,reverse=True))
 
-def lazy_sum(*args):
-    def sum():
-        ax=0
-        for n in args:
-            ax+=n
-        return ax
-    return sum
+# def lazy_sum(*args):
+#     def sum():
+#         ax=0
+#         for n in args:
+#             ax+=n
+#         return ax
+#     return sum
+#
+# f=lazy_sum(1,2,3,4,5)
+# print(f)
+# print(f())
 
-f=lazy_sum(1,2,3,4,5)
-print(f)
-print(f())
+# def count():
+#     fs=[]
+#     for i in range(1,4):
+#         def f():
+#             return i*i
+#         fs.append(f)
+#     return fs
+#
+# f1,f2,f3=count()
+# print(f1(),f2(),f3())
 
+# 返回函数不要引用任何循环变量，或者后续会发生变化的变量
+#  def count():
+#     def f(j):
+#         def g():
+#             return j*j
+#         return g
+#     fs=[]
+#     for i in range(1,4):
+#         fs.append(f(i))  #f(i)立刻被执行，因此i的当前值被传入f()
+#     return fs
+#
+# f1,f2,f3=count()
+# print(f1(),f2(),f3())
 
+# print(list(map(lambda x:x*x,[1,2,3,4,5,6,7,8,9])))
+#
+# def build(x,y):
+#     return lambda:x*x+y*y
+#
+# print(build(3,4)())
 
+#装饰器
+def log(func):
+    def wrapper(*args,**kw):
+        print('call %s函数:' % func.__name__)
+        return func(*args,*kw)
+    return wrapper
+
+@log
+def now():
+    print('2017-06-13')
+
+now()
 
 
