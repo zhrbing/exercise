3be59cd5afd47bfa636e7e0e976143d6d56a78cd
zhbing <zhrbing@gmail.com>
2017-07-12 14:59:45 +0800

    进程与线程

---
+++ zhbing004.py
@@ -0,0 +1,30 @@
+# -*- coding:utf-8 -*-
+# __author__ = 'zhbing'
+
+# import os
+# print('Process (%s) start...' % os.getpid())
+#
+# pid=os.fork()   #Windows没有fork调用
+# if pid==0:
+#     print('我是子进程(%s),我的父进程是%s' % (os.getpid(),os.getppid()))
+# else:
+#     print('我(%s)创建了一个子进程(%s)' % (os.getpid(),pid))
+
+#########################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
