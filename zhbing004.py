4ed2901f58498e79786f8cd6f107681425aa3141
zhbing <zhrbing@gmail.com>
2017-07-14 10:34:05 +0800

    正则表达式

--- zhbing004.py
+++ zhbing004.py
@@ -115,5 +115,9 @@ import re
 # m= re.match(r'^(0[0-9]|1[0-9]|2[0-3]|[0-9])\:(0[0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9]|[0-9])\:(0[0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9]|[0-9])$', t)
 # print(m.groups())
 
-print(re.match(r'^(\d+)(0*)$','102300').groups())  #贪婪匹配,\d+直接把后面的0全部匹配了，结果0*只能匹配空字符串了
-print(re.match(r'^(\d+?)(0*)$','102300').groups())  #非贪婪匹配
+# print(re.match(r'^(\d+)(0*)$','102300').groups())  #贪婪匹配,\d+直接把后面的0全部匹配了，结果0*只能匹配空字符串了
+# print(re.match(r'^(\d+?)(0*)$','102300').groups())  #加?:非贪婪匹配
+
+re_telephone=re.compile(r'^(\d{3})-(\d{3,8})$')  #编译
+print(re_telephone.match('010-12345').groups())
+print(re_telephone.match('010-8086').groups())
